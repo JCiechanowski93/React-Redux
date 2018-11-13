c180173cf3477f46cc828bd3d36ecfdf3e015688
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-11-13 20:21:41 +0100

    React App

--- js/app.jsx
+++ js/app.jsx
@@ -1,36 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay.jsx';
+import Spinner from './Spinner.jsx';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: '' };
 
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({
-                    lat: position.coords.latitude
-                });
-            },
-            err => {
-                this.setState({
-                    errorMessage: err.message
-                });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
-
-
     render() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>;
+        const { errorMessage, lat } = this.state;
+        if (errorMessage && !lat) {
+            return <div>Error: {errorMessage}</div>;
         }
-        if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+        if (!errorMessage && lat) {
+            return <SeasonDisplay lat={lat} />
         }
-        return <div>Loading...</div>;
+        return <Spinner />;
     }
 }
 
