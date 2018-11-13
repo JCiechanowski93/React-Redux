c1fd4eccf1c3cbe6fe5033cd39e744b9163bc7a8
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-11-13 11:18:04 +0100

    Comments section

--- js/app.jsx
+++ js/app.jsx
@@ -6,12 +6,21 @@ import CommentDetail from './CommentDetail.jsx'
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                content="Nice blog post"
+            />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2:00AM"
+                content="I like the subject"
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Yesterday at 5:00PM"
+                content="I like the writing"
+            />
         </div>
     );
 };
