31aa5bbc94cba365f7f148394ea58e4112845e29
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-11-13 14:11:52 +0100

    Geolocation

--- js/app.jsx
+++ js/app.jsx
@@ -1,39 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './Components/CommentDetail.jsx'
-import ApprovalCard from './Components/ApprovalCard.jsx'
 
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <CommentDetail
-                    author="Sam"
-                    timeAgo="Today at 4:45PM"
-                    content="Nice blog post"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author="Alex"
-                    timeAgo="Today at 2:00AM"
-                    content="I like the subject"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author="Jane"
-                    timeAgo="Yesterday at 5:00PM"
-                    content="I like the writing"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-        </div>
-    );
-};
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
+        return <div>Latitude: </div>
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     ReactDOM.render(
