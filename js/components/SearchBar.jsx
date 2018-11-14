ffd1e00493104c7715daf5fab06b1764706e81b5
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-11-14 10:17:47 +0100

    Commit

--- js/components/SearchBar.jsx
+++ js/components/SearchBar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = {}
-
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    state = { term: '' }
 
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
-                        <label htmlFor="">Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value })}
+                        />
                     </div>
                 </form>
             </div>
