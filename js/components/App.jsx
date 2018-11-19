328814ded7e5c6b4c22ae56794f4aa00341e8acd
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-11-19 07:44:42 +0100

    Weather List container

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar.jsx';
+import SearchBar from '../containers/SearchBar.jsx';
+import WeatherList from '../containers/WeatherList.jsx';
 
 class App extends Component {
 
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
