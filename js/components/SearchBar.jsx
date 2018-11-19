39c5e8b83dbf096710e1e0aebddb2c9bd6a42ce4
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-11-19 07:19:56 +0100

    Weather Reducer

--- js/components/SearchBar.jsx
+++ js/components/SearchBar.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeater } from '../actions/index';
 
 class SearchBar extends Component {
+
     state = { term: '' };
+
+
 
     onInputChange(event) {
         this.setState({
@@ -11,11 +17,14 @@ class SearchBar extends Component {
 
     onFormSubmit(e) {
         e.preventDefault();
+
+        this.props.fetchWeater(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
         return (
-            <form onSubmit={this.onFormSubmit} className="input-group">
+            <form onSubmit={(e) => this.onFormSubmit(e)} className="input-group">
                 <input
                     placeholder="Get a five-day forecast in your favourite cities"
                     className="form-control"
@@ -30,4 +39,10 @@ class SearchBar extends Component {
     }
 }
 
-export default SearchBar;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeater }, dispatch);
+}
+
+
+export default connect(null, mapDispatchToProps)(SearchBar);
+
