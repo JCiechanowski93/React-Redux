531ede574d8ab3f045a4d307a0c7d8ca5e750f70
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-11-20 07:32:15 +0100

    Router project

--- js/containers/WeatherList.jsx
+++ js/containers/WeatherList.jsx
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/Chart.jsx';
 
-
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const tempsToCelsius = temps.map(temp => Math.round((temp - 273.15)));
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td><Chart data={temps} color="orange" /></td>
-                <td><Chart data={pressures} color="green" /></td>
-                <td><Chart data={humidities} color="black" /></td>
+                <td><Chart data={tempsToCelsius} color="orange" units="℃" /></td>
+                <td><Chart data={pressures} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         );
     }
@@ -26,9 +26,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (℃)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
